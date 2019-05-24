74216cde94931c72dfd15a80c43dfc6b917326a8
emrullahsahin <emrullah.sahin@enuygun.com>
2019-05-24 23:30:35 +0300

    migrations

---
+++ src/Migrations/Version20190524175527.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190524175527 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_user ADD address LONGTEXT DEFAULT NULL, ADD country_id INT DEFAULT NULL, ADD city_id INT DEFAULT NULL, ADD town_id INT DEFAULT NULL, ADD phone_number VARCHAR(255) DEFAULT NULL, ADD company_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_user DROP address, DROP country_id, DROP city_id, DROP town_id, DROP phone_number, DROP company_name');
+    }
+}
