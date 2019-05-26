6a1c594e508c4a95312e77724e35f28d42a1bf2c
Yusuf İlker <yusufilkeroguz@gmail.com>
2019-05-26 21:40:00 +0300

    routes added. create-ticket added.

--- src/App/Controller/DefaultController.php
+++ src/App/Controller/DefaultController.php
@@ -17,4 +17,40 @@ class DefaultController extends Controller
     {
         return $this->render('homepage.html.twig');
     }
+
+    /**
+     * @Route("/invoice", name="invoice")
+     * @return Response
+     */
+    public function invoinceAction()
+    {
+        return $this->render('invoice.html.twig');
+    }
+
+    /**
+     * @Route("/add-balance", name="add-balance")
+     * @return Response
+     */
+    public function addBalanceAction()
+    {
+        return $this->render('add-balance.html.twig');
+    }
+
+    /**
+     * @Route("/add-credit-card", name="add-credit-card")
+     * @return Response
+     */
+    public function addCreditCardAction()
+    {
+        return $this->render('add-credit-card.html.twig');
+    }
+
+    /**
+     * @Route("/create-ticket", name="create-ticket")
+     * @return Response
+     */
+    public function createTicket()
+    {
+        return $this->render('add-credit-card.html.twig');
+    }
 }
