31abdcf647f099cf17adac3fb05d47a5a937f01a
emrullahsahin <emrullah.sahin@enuygun.com>
2019-05-27 10:42:58 +0300

    controller edit

--- src/Admin/Controller/TicketController.php
+++ src/Admin/Controller/TicketController.php
@@ -2,7 +2,10 @@
 
 namespace App\Admin\Controller;
 
+use App\Admin\Entity\Ticket;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,18 +15,41 @@ class TicketController extends EasyAdminController
     /**
      * @Route(path="/post-ticket", name="post_ticket", methods={"POST"})
      * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
      */
-    public function createTicket(Request $request){
-        $fields = $request->request;
+    public function createNewTicket(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $department = $request->request->get('departman');
+        $subject = $request->request->get('subject');
+        $content = $request->request->get('detail');
+
+        $file = $request->files->all()['file'];
+
+        $ticket = (new Ticket())
+            ->setDepartment($department)
+            ->setSubject($subject)
+            ->setContent($content)
+            ->setStatus(Ticket::STATUS_NEW);
+
+        $entityManager->persist($ticket);
+        $entityManager->flush();
+
+        return new JsonResponse([
+            'status' => true
+        ]);
     }
 
     /**
      * @Route("/all-tickets", name="all_tickets")
+     * @param Request $request
      * @return Response
      */
-    public function getAllTickets()
+    public function getAllTickets(Request $request): Response
     {
-        return $this->render('all-tickets.html.twig');
+        $isCreate = $request->query->get('create');
+
+        return $this->render('all-tickets.html.twig',['create' => $isCreate]);
     }
 
     /**
